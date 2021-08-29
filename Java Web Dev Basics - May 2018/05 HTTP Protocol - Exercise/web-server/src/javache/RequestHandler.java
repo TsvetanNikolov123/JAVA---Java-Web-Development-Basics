f2cfe40a97a16836a15bcb242494dc43b8d61d6a
TsvetanNikolov123 <tsdman1985@gmail.com>
2021-08-29 12:43:26 +0300

    Solution to exercise for HTTP protocols

---
+++ Java Web Dev Basics - May 2018/05 HTTP Protocol - Exercise/web-server/src/javache/RequestHandler.java
@@ -0,0 +1,51 @@
+package javache;
+
+import javache.http.*;
+
+public class RequestHandler {
+    private HttpRequest httpRequest;
+    private HttpResponse httpResponse;
+
+    public RequestHandler(){
+
+    }
+
+    public byte[] handleRequest(String requestContent) {
+        this.httpRequest = new HttpRequestImpl(requestContent);
+        this.httpResponse = new HttpResponseImpl();
+
+
+        return this.Ok(new byte[0]);
+    }
+
+    private byte[] Ok(byte[] content){
+        this.httpResponse.setStatusCode(HttpStatus.Ok);
+        this.httpResponse.setContent(content);
+        return this.httpResponse.getBytes();
+    }
+
+    private byte[] badRequest(byte[] content) {
+        this.httpResponse.setStatusCode(HttpStatus.BadRequest);
+        this.httpResponse.setContent(content);
+        return this.httpResponse.getBytes();
+    }
+
+    private byte[] notFound(byte[] content) {
+        this.httpResponse.setStatusCode(HttpStatus.NotFound);
+        this.httpResponse.setContent(content);
+        return this.httpResponse.getBytes();
+    }
+
+    private byte[] redirect(byte[] content, String location) {
+        this.httpResponse.setStatusCode(HttpStatus.SeeOther);
+        this.httpResponse.addHeader("Location", location);
+        this.httpResponse.setContent(content);
+        return this.httpResponse.getBytes();
+    }
+
+    private byte[] internalServerError(byte[] content) {
+        this.httpResponse.setStatusCode(HttpStatus.InternalServerError);
+        this.httpResponse.setContent(content);
+        return this.httpResponse.getBytes();
+    }
+}
