e4866349ec821f1a044800709f2ab44aa757ed38
TsvetanNikolov123 <tsdman1985@gmail.com>
2021-08-29 16:39:28 +0300

    finishing required task

--- Java Web Dev Basics - May 2018/05 HTTP Protocol - Exercise/web-server/src/javache/RequestHandler.java
+++ Java Web Dev Basics - May 2018/05 HTTP Protocol - Exercise/web-server/src/javache/RequestHandler.java
@@ -2,11 +2,17 @@ package javache;
 
 import javache.http.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class RequestHandler {
+    private static final String HTML_EXTENSION_AND_SEPARATOR = ".html";
     private HttpRequest httpRequest;
     private HttpResponse httpResponse;
 
-    public RequestHandler(){
+    public RequestHandler() {
 
     }
 
@@ -14,11 +20,14 @@ public class RequestHandler {
         this.httpRequest = new HttpRequestImpl(requestContent);
         this.httpResponse = new HttpResponseImpl();
 
+        if (this.httpRequest.getMethod().equals("GET")) {
+            return this.processGetRequest();
+        }
 
-        return this.Ok(new byte[0]);
+        return this.ok(new byte[0]);
     }
 
-    private byte[] Ok(byte[] content){
+    private byte[] ok(byte[] content) {
         this.httpResponse.setStatusCode(HttpStatus.Ok);
         this.httpResponse.setContent(content);
         return this.httpResponse.getBytes();
@@ -47,5 +56,76 @@ public class RequestHandler {
         this.httpResponse.setStatusCode(HttpStatus.InternalServerError);
         this.httpResponse.setContent(content);
         return this.httpResponse.getBytes();
+    }
+
+    private byte[] processResourceRequest() {
+        String assetPath = WebConstants.ASSETS_FOLDER_PATH + this.httpRequest.getRequestUrl();
+        File file = new File(assetPath);
+
+        if (!file.exists() || file.isDirectory()) {
+            return this.notFound(("Asset not found!").getBytes());
+        }
+
+        byte[] result = null;
+
+        try {
+            result = Files.readAllBytes(Paths.get(assetPath));
+        } catch (IOException e) {
+            return this.internalServerError(("Something went wrong!").getBytes());
+        }
+
+        this.httpResponse.addHeader("Content-Type", this.getMimeType(file));
+        this.httpResponse.addHeader("Content-Length", result.length + "");
+        this.httpResponse.addHeader("Content-Disposition", "inline");
+
+        return this.ok(result);
+    }
+
+    private String getMimeType(File file) {
+        String fileName = file.getName();
+
+        if (fileName.endsWith("css")) {
+            return "text/css";
+        } else if (fileName.endsWith("html")) {
+            return "text/html";
+        } else if (fileName.endsWith("jpg") || fileName.endsWith("jpeg")) {
+            return "image/jpeg";
+        } else if (fileName.endsWith("png")) {
+            return "image/png";
+        }
+
+        return "text/plain";
+    }
+
+    private byte[] processPageRequest(String page) {
+        String pagePath = WebConstants.PAGES_FOLDER_PATH
+                + page
+                + HTML_EXTENSION_AND_SEPARATOR;
+        File file = new File(pagePath);
+
+        if (!file.exists() || file.isDirectory()) {
+            return this.notFound(("Page not found!").getBytes());
+        }
+
+        byte[] result = null;
+
+        try {
+            result = Files.readAllBytes(Paths.get(pagePath));
+        } catch (IOException e) {
+            return this.internalServerError(("Something went wrong!").getBytes());
+        }
+
+        this.httpResponse.addHeader("Content-Type", this.getMimeType(file));
+        return this.ok(result);
+    }
+
+    private byte[] processGetRequest() {
+        if (this.httpRequest.getRequestUrl().equals("/")) {
+            return this.processPageRequest("/index");
+        } else if (this.httpRequest.getRequestUrl().equals("/login")) {
+            return this.processPageRequest(this.httpRequest.getRequestUrl());
+        }
+
+        return this.processResourceRequest();
     }
 }
